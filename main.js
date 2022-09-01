dc23ce405ec0fd75a6cd8bbb2335e193f14600f6
csslick <digitone@naver.com>
2022-09-01 23:17:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 // main.js
-const pageable = new Pageable("#container");
+
+const pageable = new Pageable("#container", {
+  onFinish: function(data) {
+    // 스크롤 완료
+    console.log('scroll finish ', data.index)
+
+    // 전체 page aos 요소 애니메이션 초기화
+    let pages = document.querySelectorAll('section');
+    pages.forEach(page => {
+      const h1 = page.querySelector('div h1')
+      h1.classList.remove('aos-animate')
+    })
+
+    // 현재 page의 요소 애니메이션
+    const h1 = pages[data.index].querySelector(`div h1`)
+    h1.classList.add('aois-init', 'aos-animate')
+
+},
+});
 
